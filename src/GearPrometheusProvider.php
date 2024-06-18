c3df63f831d0c120610b8dd51c4e182479b665ae
vladimir zakotnev <vladitot@gmail.com>
2024-06-18 18:03:22 +0200

    fix prometheus

--- src/GearPrometheusProvider.php
+++ src/GearPrometheusProvider.php
@@ -5,11 +5,18 @@ namespace GearDev\Prometheus;
 
 use GearDev\Collector\Collector\Collector;
 use Illuminate\Support\ServiceProvider;
+use Prometheus\CollectorRegistry;
+use Prometheus\Storage\InMemory;
 
 class GearPrometheusProvider extends ServiceProvider
 {
     public function register() {
         Collector::addPackageToCollector(__DIR__);
+
+        $collectorRegistry = new CollectorRegistry(app()->make(InMemory::class));
+        app()->singleton(CollectorRegistry::class, function() use ($collectorRegistry) {
+            return $collectorRegistry;
+        });
     }
 
     public function boot() {
